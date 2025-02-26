9baa6dc9ffe70e2916290e99c24c93602af17cc6
sandeshlavshetty <sandeshtlavshetty@gmail.com>
2025-02-26 20:57:43 +0530

    media path route added

--- routes/media_routes.py
+++ routes/media_routes.py
@@ -69,6 +69,14 @@ def delete_media_file(media_type, media_id):
     return jsonify({"error": "Media not found"}), 404
 
 
+@media_bp.route("/<string:media_path>", methods=["GET"])
+def give_image_url(media_path):
+    if media_path :
+        print(f"public url :- {os.path.join(Config.SUPABASE_STORAGE_URL,media_path)}")
+        return os.path.join(Config.SUPABASE_STORAGE_URL,media_path)
+    
+    return None
+
 # Medai table routes
 
 
