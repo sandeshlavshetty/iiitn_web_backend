0262fab81e18a54259b706f798ba8362541aaaf9
sandeshlavshetty <sandeshtlavshetty@gmail.com>
2025-03-03 19:54:51 +0530

    changed to render deploy

--- functions/netlify_handler.py
+++
@@ -1,5 +0,0 @@
-from app import app  # Your Flask app
-from netlify_handler import handler
-
-def handler(event, context):
-    return handler(app, event, context)
--- render-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install system dependencies
-apt-get update && apt-get install -y graphviz graphviz-dev
-
-# Install Python dependencies
-pip install -r requirements.txt
