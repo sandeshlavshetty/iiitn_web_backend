d10a6125cece0c0df82b106f8946819d086bf6e6
sandeshlavshetty <sandeshtlavshetty@gmail.com>
2025-03-09 21:59:04 +0530

    security headers added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response
 from database import db
 from flask_migrate import Migrate
 from routes import register_routes
@@ -14,6 +14,15 @@ from database import init_db
 load_dotenv()  # Load environment variables
 
 app = Flask(__name__)
+@app.after_request
+def add_security_headers(response):
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["Referrer-Policy"] = "no-referrer-when-downgrade"
+    response.headers["Permissions-Policy"] = "geolocation=(), microphone=()"
+    return response
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
