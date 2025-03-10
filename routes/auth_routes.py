d5c0d8b18ae5661afee3b247693f2125c6fda887
sandeshlavshetty <sandeshtlavshetty@gmail.com>
2025-03-10 20:55:47 +0530

    session error corrected

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session
 from flask import Blueprint, request, jsonify
 from werkzeug.security import check_password_hash
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
@@ -11,21 +11,6 @@ auth_bp = Blueprint("auth", __name__)
 def get_auths():
     return jsonify({"message": "auth routes working!"})
 
-
-# @auth_bp.route("/login", methods=["POST"])
-# def login():
-#     data = request.json
-#     email = data.get("email")
-#     password = data.get("password")
-
-#     user = Person.query.filter_by(email_pri=email).first()
-    
-#     if user and check_password_hash(user.password, password):
-#         # Generate JWT token
-#         access_token = create_access_token(identity={"email": user.email_pri, "role": user.role})
-#         return jsonify({"access_token": access_token}), 200
-    
-#     return jsonify({"message": "Invalid credentials"}), 401
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
