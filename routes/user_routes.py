902612432d2e69df08befe7d1148cc25083ec31e
sandeshlavshetty <sandeshtlavshetty@gmail.com>
2025-04-07 20:22:05 +0530

    social media for user updated

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
-from database.db_operations import add_person, get_person, get_all_persons, update_person, delete_person
+from database.db_operations import add_person, get_person, get_all_persons, update_person, delete_person,add_social_media
 
 
 user_bp = Blueprint("user", __name__)
@@ -9,6 +9,7 @@ user_bp = Blueprint("user", __name__)
 # # @jwt_required()  #middleware
 # def get_users():
 #     return jsonify({"message": "User routes working!"})
+
 
 @user_bp.route("/", methods=["GET"])
 def fetch_all_persons():
@@ -24,7 +25,13 @@ def fetch_all_persons():
             "curr_address": p.curr_address,
             "perm_address": p.perm_address,
             "role": p.role,
-            "sm_id": p.sm_id
+            "sm_id": p.sm_id,
+            "social_media": {
+                "insta": p.social_media.insta if p.social_media else None,
+                "twitter": p.social_media.twitter if p.social_media else None,
+                "linkedin": p.social_media.linkedin if p.social_media else None,
+                "youtube": p.social_media.youtube if p.social_media else None,
+            } if p.social_media else None
         } for p in persons
     ])
 
@@ -44,15 +51,33 @@ def fetch_person(p_id):
         "curr_address": p.curr_address,
         "perm_address": p.perm_address,
         "role": p.role,
-        "sm_id": p.sm_id
+        "sm_id": p.sm_id,
+        "social_media": {
+            "insta": p.social_media.insta if p.social_media else None,
+            "twitter": p.social_media.twitter if p.social_media else None,
+            "linkedin": p.social_media.linkedin if p.social_media else None,
+            "youtube": p.social_media.youtube if p.social_media else None,
+        } if p.social_media else None
     })
-
 @user_bp.route("/", methods=["POST"])
 def create_person():
     data = request.json
     required_fields = ["email_pri", "name", "phone_no", "password", "role"]
     if not all(field in data for field in required_fields):
         return jsonify({"error": "Missing required fields"}), 400
+
+    # Create SocialMedia record if links are given
+    sm_data = data.get("social_media", {})
+    if any(sm_data.get(key) for key in ["insta", "twitter", "linkedin", "youtube"]):
+        sm_entry = add_social_media(
+            insta=sm_data.get("insta"),
+            twitter=sm_data.get("twitter"),
+            linkedin=sm_data.get("linkedin"),
+            youtube=sm_data.get("youtube")
+        )
+        sm_id = sm_entry.sm_id
+    else:
+        sm_id = None
 
     person = add_person(
         email_pri=data["email_pri"],
@@ -64,10 +89,20 @@ def create_person():
         perm_address=data.get("perm_address"),
         password=data["password"],
         role=data["role"],
-        sm_id=data.get("sm_id")
+        sm_id=sm_id
     )
 
-    return jsonify({"message": "Person added", "p_id": person.p_id})
+    return jsonify({
+        "message": "Person added",
+        "p_id": person.p_id,
+        "social_media": {
+            "insta": person.social_media.insta if person.social_media else None,
+            "twitter": person.social_media.twitter if person.social_media else None,
+            "linkedin": person.social_media.linkedin if person.social_media else None,
+            "youtube": person.social_media.youtube if person.social_media else None,
+        } if person.social_media else None
+    })
+
 
 @user_bp.route("/<int:p_id>", methods=["PUT"])
 def modify_person(p_id):
