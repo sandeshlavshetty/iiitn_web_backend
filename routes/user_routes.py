f8212f3e3f89894ddf3fefb7dcc15e1937208911
sandeshlavshetty <sandeshtlavshetty@gmail.com>
2025-04-10 20:28:04 +0530

    USER PATCH Request

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -100,7 +100,7 @@ def create_person():
         } if person.social_media else None
     })
 
-@user_bp.route("/<int:p_id>", methods=["PUT"])
+@user_bp.route("/<int:p_id>", methods=["PATCH"])
 def modify_person(p_id):
     data = request.json
     person = update_person(p_id, **data)
