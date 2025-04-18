d62de8c62081c8c8445da3eebb434ff273e5aff8
sandeshlavshetty <sandeshtlavshetty@gmail.com>
2025-04-18 18:07:57 +0530

    removed csp from backend

--- app.py
+++ app.py
@@ -14,14 +14,7 @@ from utils.error_handler import register_error_handlers  # ✅ Import error hand
 load_dotenv()  # Load environment variables
 
 app = Flask(__name__)
-@app.after_request
-def add_security_headers(response):
-    response.headers["Content-Security-Policy"] = "default-src 'self'"
-    response.headers["X-Frame-Options"] = "DENY"
-    response.headers["X-Content-Type-Options"] = "nosniff"
-    response.headers["Referrer-Policy"] = "no-referrer-when-downgrade"
-    response.headers["Permissions-Policy"] = "geolocation=(), microphone=()"
-    return response
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
